aa8cd83c7b05fb4a84eb94e092ce766ef43d85fd
delfidupi <delfina.dupiellet@ing.austral.edu.ar>
2024-06-03 22:34:30 -0300

    Update Matches.py

--- pages/Matches.py
+++ pages/Matches.py
@@ -4,17 +4,6 @@ from funciones import dni_exists, match
 if "Usuario_encontrado" not in st.session_state:
     st.session_state["Usuario_encontrado"] = False
 
-st.title("Ingreso de usuario")
-dni = st.text_input("DNI")
-
-if st.button('Buscar'):
-    if dni_exists(dni):
-        st.session_state["Usuario_encontrado"] = True
-        st.session_state["dni"]= dni
-    else:
-        st.error("Usuario no encontrado") 
-        st.session_state["Usuario_encontrado"] =False
-
 if st.session_state.get("Usuario_encontrado", False):
     st.subheader('Los puestos disponibles que coinciden con tu perfil son:')
     puestos = match(st.session_state["dni"])  # Asegúrate de que esta función ejecute la consulta SQL y devuelva un DataFrame con las columnas nombre, descripcion y contacto
