b6fce92dfcc5ddd6d68192ad4c21e5701451a9e5
delfidupi <delfina.dupiellet@ing.austral.edu.ar>
2024-06-06 22:13:03 -0300

    Update Matches.py

--- pages/Matches.py
+++ pages/Matches.py
@@ -13,6 +13,17 @@ if st.button('Buscar'):
     if dni_exists(dni):
         st.session_state["Usuario_encontrado"] = True
         st.session_state["dni"]= dni
+        if st.session_state.get("Usuario_encontrado", False):
+            st.subheader('Los puestos disponibles que coinciden con tu perfil son:')
+            puestos = match(st.session_state["dni"])  # Asegúrate de que esta función ejecute la consulta SQL y devuelva un DataFrame con las columnas nombre, descripcion y contacto
+            if not puestos.empty:  # Check if the DataFrame is not empty
+                for index, row in puestos.iterrows():
+                    st.write(f"Nombre: {row['nombre']}")
+                    st.write(f"Descripción: {row['descripcion']}")
+                    st.write(f"Contacto: {row['contacto']}")
+                    st.write("---")  # Separador entre puestos
+            else:
+                st.write("No se encontraron puestos disponibles.")
     else:
         st.error("Usuario no encontrado") 
         st.session_state["Usuario_encontrado"] =False
