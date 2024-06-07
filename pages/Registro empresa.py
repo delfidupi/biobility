13bcf28b7c7df2450021155d252ab518953ea61b
delfidupi <delfina.dupiellet@ing.austral.edu.ar>
2024-06-06 22:37:29 -0300

    Update Registro empresa.py

--- pages/Registro empresa.py
+++ pages/Registro empresa.py
@@ -1,5 +1,7 @@
 import streamlit as st
 from funciones import insertEmpresa
+import time 
+from streamlit_extras.switch_page_button import switch_page
 
 st.session_state["Empresa encontrado"] = False
 st.title("Registrar Empresa")
