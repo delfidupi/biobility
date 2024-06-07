6785f3797041a4450db60de0d78b5fa82162f99a
delfidupi <delfina.dupiellet@ing.austral.edu.ar>
2024-06-06 22:17:25 -0300

    Update Profesional usuario.py

--- pages/Profesional usuario.py
+++ pages/Profesional usuario.py
@@ -14,29 +14,28 @@ if st.button('Buscar'):
     if dni_exists(dni):
         st.session_state["Usuario_encontrado"] = True
         st.session_state["dni"] = dni
-        if st.session_state.get("Usuario_encontrado", False):
-            st.subheader('Carga tus datos profesionales')
-            especialidad = st.selectbox("Especialidad: Bioinformatica (B), Protesis (P), Genética (G), Imagenes biomedicas (I)", ("B", "P", "G", "I"))
-            turno = st.selectbox("Turno: Mañana (M), Tarde (T), Noche (N)", ("M", "T", "N"))
-            presencialidad = st.selectbox("Presencialidad", ("Si", "No"))
-            estudios = st.text_input("¿Donde te recibiste?")
-            experienciaLaboral = st.selectbox("¿Tenes experiencia laboral?", ("Si", "No"))
-    
-            # Mostrar botón "Guardar" después de completar los datos
-            if st.button("Guardar"):
-                if not dni or not especialidad or not turno or not presencialidad or not estudios or not experienciaLaboral:
-                    st.error("Por favor completa todos los campos")
-                else:
-                    insertCurriculum(dni, especialidad, turno, presencialidad, estudios, experienciaLaboral)
-                    st.session_state.saved = True
-                    st.session_state["Usuario_encontrado"] = False
-                    st.success("Datos guardados correctamente")
-                    with st.spinner('Aguarde para ver sus coincidencias!...'):
-                        time.sleep(5)  # Espera de 2 segundos
-                    switch_page("Matches")  # Esto debería ser reemplazado con tu lógica para cambiar de página
     else:
         st.error("Usuario no encontrado")
         st.session_state["Usuario_encontrado"] = False
 
-
-
+if st.session_state.get("Usuario_encontrado", False):
+    st.subheader('Carga tus datos profesionales')
+    especialidad = st.selectbox("Especialidad: Bioinformatica (B), Protesis (P), Genética (G), Imagenes biomedicas (I)", ("B", "P", "G", "I"))
+    turno = st.selectbox("Turno: Mañana (M), Tarde (T), Noche (N)", ("M", "T", "N"))
+    presencialidad = st.selectbox("Presencialidad", ("Si", "No"))
+    estudios = st.text_input("¿Donde te recibiste?")
+    experienciaLaboral = st.selectbox("¿Tenes experiencia laboral?", ("Si", "No"))
+    
+    # Mostrar botón "Guardar" después de completar los datos
+    if st.button("Guardar"):
+        if not dni or not especialidad or not turno or not presencialidad or not estudios or not experienciaLaboral:
+            st.error("Por favor completa todos los campos")
+        else:
+            insertCurriculum(dni, especialidad, turno, presencialidad, estudios, experienciaLaboral)
+            st.session_state.saved = True
+            st.session_state["Usuario_encontrado"] = False
+            st.success("Datos guardados correctamente")
+            with st.spinner('Aguarde para ver sus coincidencias!...'):
+                time.sleep(5)  # Espera de 2 segundos
+            switch_page("Matches")  # Esto debería ser reemplazado con tu lógica para cambiar de página
+    
