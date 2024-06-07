ec01e1bfb04f89c5ffb6bea6ee49e75a2f5b5a72
delfidupi <delfina.dupiellet@ing.austral.edu.ar>
2024-06-06 22:04:45 -0300

    Update Registro usuario.py

--- pages/Registro usuario.py
+++ pages/Registro usuario.py
@@ -11,24 +11,16 @@ nombre = st.text_input("Nombre y apellido")
 cumpleaños = st.text_input("Fecha de nacimiento (dd/mm/aaaa)")
 zona = st.selectbox("Zona de trabajo: Norte/Sur/Oeste/CABA", ("N", "O", "S", "C"))
 
-# Definir el estado de guardado
-if 'saved' not in st.session_state:
-    st.session_state.saved = False
 
 if st.button("Guardar"):
     if not dni or not nombre or not cumpleaños or not zona:
         st.error("Por favor completa todos los campos")
     else:
         insertUser(dni, nombre, cumpleaños, zona)
-        st.session_state.saved = True
-        st.success("Datos guardados correctamente")
-
-# Mostrar el botón "Crear un curriculum" solo si los datos han sido guardados
-if st.session_state.saved:
-    if st.button("Crear un curriculum"):
         with st.spinner('Cargando...'):
             time.sleep(2)  # Espera de 2 segundos
         switch_page("Profesional Usuario")
+
 
 
 
