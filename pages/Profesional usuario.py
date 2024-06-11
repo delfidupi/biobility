6e7e0c5cd7c44d7f175acb7f290dce61db55d3e5
delfidupi <delfina.dupiellet@ing.austral.edu.ar>
2024-06-10 22:56:00 -0300

    Update Profesional usuario.py

--- pages/Profesional usuario.py
+++ pages/Profesional usuario.py
@@ -6,7 +6,14 @@ from streamlit_extras.switch_page_button import switch_page
 if "Usuario_encontrado" not in st.session_state:
     st.session_state["Usuario_encontrado"] = False
     
-st.title("Ingreso de usuario")
+st.markdown(
+    """
+    <h1 style="font-family: 'Roboto', sans-serif; color: #494666;">
+        Ingreso de usuario
+    </h1>
+    """,
+    unsafe_allow_html=True
+)
 dni = st.text_input("Ingrese su DNI")
 st.markdown(
     """
