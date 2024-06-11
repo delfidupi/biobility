c7ace4659d374f0b6631fa21d36c89487093bd5b
delfidupi <delfina.dupiellet@ing.austral.edu.ar>
2024-06-10 22:52:11 -0300

    Update Registro usuario.py

--- pages/Registro usuario.py
+++ pages/Registro usuario.py
@@ -9,7 +9,7 @@ from funciones import insertUser
 st.session_state["Usuario encontrado"] = False
 st.markdown(
     """
-    <h1 style="font-family: 'Comic Sans MS', sans-serif; color: #494666;">
+    <h1 style="font-family: 'Pacifico', cursive; color: #494666;">
         Ingreso de usuario
     </h1>
     """,
